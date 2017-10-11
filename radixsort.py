dfe9a7cc393c7fe3f09e9860674208a0afccd8ad
dlcksgy <hyo517@naver.com>
2017-10-11 18:40:52 +0900

    Update radixsort.py
    first bucket list is not used.
    I think it's ok to remove it.

--- radixsort.py
+++ radixsort.py
@@ -2,7 +2,6 @@ import math
 
 def radixsort(L):
         temp = list(L)
-        bucket = [[] for _ in xrange(10)]
         for x in xrange(int(math.log(max(L), 10))+1):
                 bucket = [[] for _ in xrange(10)]
                 for l in temp:
